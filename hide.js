00abe2ff6b237f788a7702d7296c2097001a1ba1
craze anonymous <71683238+anonymous-villager@users.noreply.github.com>
2020-12-29 11:53:36 +0800

    Add files via upload

---
+++ hide.js
@@ -0,0 +1,51 @@
+function show1() {
+    var show1 = document.getElementById('show1');
+    var hide1 = document.getElementById('hide1');
+    if (hide1.style.display === 'none') {
+          hide1.style.display = 'block';
+          show1.innerText = "點擊隱藏";
+    }
+    else {
+          hide1.style.display = 'none';
+          show1.innerText = "Beat Box";
+    }
+}
+
+function show2() {
+      var show2 = document.getElementById('show2');
+      var hide2 = document.getElementById('hide2');
+      if (hide2.style.display === 'none') {
+            hide2.style.display = 'block';
+            show2.innerText = "點擊隱藏";
+      }
+      else {
+            hide2.style.display = 'none';
+            show2.innerText = "肺炎";
+      }
+  }
+
+function show3() {
+      var show3 = document.getElementById('show3');
+      var hide3 = document.getElementById('hide3');
+      if (hide3.style.display === 'none') {
+            hide3.style.display = 'block';
+            show3.innerText = "點擊隱藏";
+      }
+      else {
+            hide3.style.display = 'none';
+            show3.innerText = "最「近」";
+      }
+  }
+
+  function show4() {
+      var show4 = document.getElementById('show4');
+      var hide4 = document.getElementById('hide4');
+      if (hide4.style.display === 'none') {
+            hide4.style.display = 'block';
+            show4.innerText = "點擊隱藏";
+      }
+      else {
+            hide4.style.display = 'none';
+            show4.innerText = "最「近」";
+      }
+  }
