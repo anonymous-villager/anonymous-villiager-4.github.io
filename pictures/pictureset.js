00348bf6308be375bfef8b2ab3e1d38bfd658c94
craze anonymous <71683238+anonymous-villager@users.noreply.github.com>
2020-12-29 11:49:13 +0800

    Add files via upload

---
+++ pictures/pictureset.js
@@ -0,0 +1,117 @@
+function show1() {
+    var show1 = document.getElementById('show1');
+    var hide1 = document.getElementById('hide1');
+    if (hide1.style.display === 'none') {
+          hide1.style.display = 'block';
+          show1.innerText = "點擊隱藏";
+    }
+    else {
+          hide1.style.display = 'none';
+          show1.innerText = "Beat Box";
+    }
+}
+
+function show2() {
+      var show2 = document.getElementById('show2');
+      var hide2 = document.getElementById('hide2');
+      if (hide2.style.display === 'none') {
+            hide2.style.display = 'block';
+            show2.innerText = "點擊隱藏";
+      }
+      else {
+            hide2.style.display = 'none';
+            show2.innerText = "肺炎";
+      }
+  }
+
+  function show3() {
+      var show3 = document.getElementById('show3');
+      var hide3 = document.getElementById('hide3');
+      if (hide3.style.display === 'none') {
+            hide3.style.display = 'block';
+            show3.innerText = "點擊隱藏";
+      }
+      else {
+            hide3.style.display = 'none';
+            show3.innerText = "肺炎";
+      }
+  }
+
+
+function show4() {
+      var show4 = document.getElementById('show4');
+      var hide4 = document.getElementById('hide4');
+      if (hide4.style.display === 'none') {
+            hide4.style.display = 'block';
+            show4.innerText = "點擊隱藏";
+      }
+      else {
+            hide4.style.display = 'none';
+            show4.innerText = "小孩の困擾";
+      }
+  }
+
+function show5() {
+    var show5 = document.getElementById('show5');
+    var hide5 = document.getElementById('hide5');
+    if (hide5.style.display === 'none') {
+          hide5.style.display = 'block';
+          show5.innerText = "點擊隱藏";
+    }
+    else {
+          hide5.style.display = 'none';
+          show5.innerText = "不知所措";
+    }
+}
+
+function show6() {
+      var show6 = document.getElementById('show6');
+      var hide6 = document.getElementById('hide6');
+      if (hide6.style.display === 'none') {
+            hide6.style.display = 'block';
+            show6.innerText = "點擊隱藏";
+      }
+      else {
+            hide6.style.display = 'none';
+            show6.innerText = "肺炎";
+      }
+  }
+
+function show7() {
+      var show7 = document.getElementById('show7');
+      var hide7 = document.getElementById('hide7');
+      if (hide7.style.display === 'none') {
+            hide7.style.display = 'block';
+            show7.innerText = "點擊隱藏";
+      }
+      else {
+            hide7.style.display = 'none';
+            show7.innerText = "加班";
+      }
+  }
+
+function show8() {
+      var show8 = document.getElementById('show8');
+      var hide8= document.getElementById('hide8');
+      if (hide8.style.display === 'none') {
+            hide8.style.display = 'block';
+            show8.innerText = "點擊隱藏";
+      }
+      else {
+            hide8.style.display = 'none';
+            show8.innerText = "半聾";
+      }
+  }
+  
+function show9() {
+      var show9 = document.getElementById('show9');
+      var hide9= document.getElementById('hide9');
+      if (hide9.style.display === 'none') {
+            hide9.style.display = 'block';
+            show9.innerText = "點擊隱藏";
+      }
+      else {
+            hide9.style.display = 'none';
+            show9.innerText = "半聾";
+      }
+  }  
